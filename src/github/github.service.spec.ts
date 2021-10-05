a9846c2b2dd6dcece59a26133e3b9a48b8f0a1a6
Christian <you@example.com>
2021-10-05 10:38:39 +0200

    resolved tests and added testing pipeline

--- src/github/github.service.spec.ts
+++ src/github/github.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GithubService } from './github.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
 
 describe('GithubService', () => {
   let service: GithubService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GithubService],
+      providers: [GithubService, ConfigService],
+      imports: [HttpModule],
     }).compile();
 
     service = module.get<GithubService>(GithubService);
