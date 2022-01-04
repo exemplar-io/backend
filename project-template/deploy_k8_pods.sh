e333dca91151dec412ccadd9a9af62625cd7c455
Christian <christianhjelmslund@gmail.com>
2022-01-04 11:35:10 +0100

    start minikube tunnel

--- project-template/deploy_k8_pods.sh
+++ project-template/deploy_k8_pods.sh
@@ -4,4 +4,4 @@ eval $(minikube docker-env)
 kubectl apply -f deployments/deploy-redis.yaml
 kubectl apply -f deployments/deploy-api.yaml
 kubectl apply -f deployments/deploy-ms.yaml
-
+minikube tunnel &> /dev/null &
