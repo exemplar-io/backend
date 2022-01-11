fcd927718b16a5e4b55b6f2fb0b49979a2c399e5
sasp1 <sebastian.a.specht@gmail.com>
2022-01-11 20:52:23 +0100

    increased time between health check curl requests

--- project-template/wait_for_it.sh
+++ project-template/wait_for_it.sh
@@ -3,7 +3,7 @@
 echo "Waiting app to launch on 3000..."
 
 while ! curl localhost:3000; do   
-  sleep 1 # wait for 1s of the second before check again
+  sleep 5 # wait for 5s of the second before check again
 done
 
 echo "App launched"
