82f4136b9ed3f666977736489fd35cc192ff8245
sasp1 <sebastian.a.specht@gmail.com>
2022-01-17 12:12:03 +0100

    removed whitespace from .github workflow file

--- project-template/frontend/src/e2e/test/specs/example.e2e.js
+++ project-template/frontend/src/e2e/test/specs/example.e2e.js
@@ -1,9 +1,11 @@
 const HomePage = require('../pageobjects/home.page');
 
 describe('Home Page', () => {
-  it('Should display login button', async () => {
+  it('Should display login success text', async () => {
     await HomePage.open();
-    await HomePage.login("admin", "password");
+    await HomePage.$('#username').setValue(username);
+    await HomePage.$('#password').setValue(password);
+    await HomePage.$('.submit').click();
     await expect(HomePage.confirmationText).toHaveTextContaining("Logged in!")
   });
 });
