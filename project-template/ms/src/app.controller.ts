cf32b2111b1acf41e9d6e6390d76590a2e0c5a17
Christian <christianhjelmslund@gmail.com>
2022-01-28 13:56:50 +0100

    fix

--- project-template/ms/src/app.controller.ts
+++ project-template/ms/src/app.controller.ts
@@ -6,15 +6,9 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-<<<<<<< HEAD
 
   @MessagePattern('get-hello')
   getHello(): string {
     return this.appService.getHello();
-=======
-  @MessagePattern('empty-ms')
-  returnSomeMessage(): string {
-    return this.appService.returnSomeMessage();
->>>>>>> 1390e4d173a9f5df15d0adf7b281608688124909
   }
 }
