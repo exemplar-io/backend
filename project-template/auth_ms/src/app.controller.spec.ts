428a4989b9737c8392991bc284e342f0ea65ae2a
Christian <christianhjelmslund@gmail.com>
2022-01-28 14:29:55 +0100

    fix tests

--- project-template/auth_ms/src/app.controller.spec.ts
+++ project-template/auth_ms/src/app.controller.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -9,21 +8,9 @@ describe('AppController', () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [
-        {
-          provide: AppService,
-          useValue: {
-            getHello: jest.fn(() => 'Hello World!'),
-          },
-        },
       ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
-  });
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
   });
 });
