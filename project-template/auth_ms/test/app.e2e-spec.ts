568112711fb71406f6452ce23b5ea2e291f96be9
sasp1 <sebastian.a.specht@gmail.com>
2022-01-31 21:33:54 +0100

    fixed testing in backend

--- project-template/auth_ms/test/app.e2e-spec.ts
+++ project-template/auth_ms/test/app.e2e-spec.ts
@@ -1,28 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { ClientProxy, ClientsModule, Transport } from '@nestjs/microservices';
-import { lastValueFrom, Observable } from 'rxjs';
+import { catchError, lastValueFrom, of } from 'rxjs';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
   let client: ClientProxy;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule, ClientsModule.register([
-        { name: 'MS', transport: Transport.TCP },
-      ]),],
+      imports: [
+        AppModule,
+        ClientsModule.register([{ name: 'MS', transport: Transport.TCP }]),
+      ],
     }).compile();
     app = moduleFixture.createNestApplication();
-    
     app.connectMicroservice({
       transport: Transport.TCP,
     });
-    
-    await app.startAllMicroservices();
 
+    await app.startAllMicroservices();
 
     client = app.get('MS');
     await client.connect();
@@ -35,12 +33,28 @@ describe('AppController (e2e)', () => {
   });
 
   it('Should login successfully', async () => {
-    const response = await lastValueFrom(client.send(
-      "login",
-      { username: "admin", password: "password" },
-    ));
+    const response = await lastValueFrom(
+      client.send('login', { username: 'admin', password: 'password' }),
+    );
 
-    expect(typeof response).toBe("string");
+    expect(typeof response).toBe('string');
     expect(response.length).toBeGreaterThan(10);
+  });
+
+  it('Should throw unauthorized exception', (done) => {
+    lastValueFrom(
+      client
+        .send('login', {
+          username: 'wrong',
+          password: 'credentials',
+        })
+        .pipe(
+          catchError((err) => {
+            expect(err.status).toEqual('error');
+            done();
+            return of({});
+          }),
+        ),
+    );
   });
 });
