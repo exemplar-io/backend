c3208965a918532c2b8ecc658311e7e59bb20c51
sasp1 <sebastian.a.specht@gmail.com>
2022-01-31 18:20:55 +0100

    added e2e test in api

--- project-template/auth_ms/test/app.e2e-spec.ts
+++ project-template/auth_ms/test/app.e2e-spec.ts
@@ -2,23 +2,45 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { ClientProxy, ClientsModule, Transport } from '@nestjs/microservices';
+import { lastValueFrom, Observable } from 'rxjs';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let client: ClientProxy;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, ClientsModule.register([
+        { name: 'MS', transport: Transport.TCP },
+      ]),],
     }).compile();
-
     app = moduleFixture.createNestApplication();
+    
+    app.connectMicroservice({
+      transport: Transport.TCP,
+    });
+    
+    await app.startAllMicroservices();
+
+
+    client = app.get('MS');
+    await client.connect();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+    client.close();
+  });
+
+  it('Should login successfully', async () => {
+    const response = await lastValueFrom(client.send(
+      "login",
+      { username: "admin", password: "password" },
+    ));
+
+    expect(typeof response).toBe("string");
+    expect(response.length).toBeGreaterThan(10);
   });
 });
