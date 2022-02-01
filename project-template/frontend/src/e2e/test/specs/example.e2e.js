49581af02787bbcc71e438ed48b3b90fff9acf8e
sasp1 <sebastian.a.specht@gmail.com>
2022-02-01 17:30:21 +0100

    fixed frontend e2e test

--- project-template/frontend/src/e2e/test/specs/example.e2e.js
+++ project-template/frontend/src/e2e/test/specs/example.e2e.js
@@ -4,7 +4,6 @@ describe('Home Page', () => {
   it('Should display login success text', async () => {
     await HomePage.open();
     await HomePage.login('admin', 'password');
-    await HomePage.$('.submit').click();
     await expect(HomePage.confirmationText).toHaveTextContaining('Logged in!');
   });
 });
